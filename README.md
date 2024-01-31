79bf828fbff603c0d952ba4cde07dd7424a822d1
johnpurnell <johnpurnell@users.noreply.github.com>
2024-01-31 11:44:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-fxm1no
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/johnpurnell/stackblitz-starters-fxm1no)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, StackBlitz and Github</h1>
+      <p>
+        ho <b>humm</b>{' '}
+      </p>
+    </div>
+  );
+}
